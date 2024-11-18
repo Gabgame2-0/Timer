d81d0b6940681c9bcb942a4e4406dbd789918d99
Amara Djaodo <Amar95280@outlook.com>
2024-11-18 18:14:50 +0100

    code begging

--- Program.cs
+++ Program.cs
@@ -6,8 +6,23 @@ namespace Timer
     {
         public static void Main(string[] args)
         {
-            //code beginning
-            Console.WriteLine("All is Ok and none");
+            Console.CursorVisible = false;
+            bool playing = true;
+            int hour = 0;
+            int minute = 0;
+            int second = 0;
+            while (playing)
+            {
+                //code begging
+                Console.WriteLine("00:00:00     Press enter to start !");
+                ConsoleKeyInfo touch = Console.ReadKey(intercept: true);
+                if (touch.Key == ConsoleKey.Enter)
+                { }
+                else if (touch.Key == ConsoleKey.RightArrow)
+                { }
+
+                playing = false;
+            }
         }
     }
 }
