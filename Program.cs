e4fd32496aac3a9808ec826c1aeb387576e005fd
Amara Djaodo <Amar95280@outlook.com>
2024-11-18 17:16:33 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Timer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("All is Ok");
+        }
+    }
+}
