8c959daa5ceb8b793e15681f1e6314a337c6df2d
Amara Djaodo <Amar95280@outlook.com>
2024-11-18 19:47:09 +0100

    highligther advencement

--- Program.cs
+++ Program.cs
@@ -4,25 +4,86 @@ namespace Timer
 {
     public class Program
     {
+        public static int highlighter = -2;
+        public static int second = 0;
+        public static int minute = 0;
         public static void Main(string[] args)
         {
-            Console.CursorVisible = false;
+            Console.ForegroundColor = ConsoleColor.White;
             bool playing = true;
-            int hour = 0;
-            int minute = 0;
-            int second = 0;
+            bool choice = true;
             while (playing)
             {
-                //code begging
-                Console.WriteLine("00:00:00     Press enter to start !");
-                ConsoleKeyInfo touch = Console.ReadKey(intercept: true);
-                if (touch.Key == ConsoleKey.Enter)
-                { }
-                else if (touch.Key == ConsoleKey.RightArrow)
-                { }
 
+                Console.CursorVisible = false;
+
+
+
+
+                Console.WriteLine(@"00:00     Press ""Enter"" to start or ""Escape"" to quit!");
+                while (choice)
+                {
+                    //code begging
+                    
+                    ConsoleKeyInfo touch = Console.ReadKey(intercept: true);
+                    //manage input
+                    if (touch.Key == ConsoleKey.Enter)
+                    { }
+                    else if (touch.Key == ConsoleKey.RightArrow)
+                    {
+                        highlighter += 2;
+                    }
+                    else if (touch.Key == ConsoleKey.LeftArrow)
+                    {
+                        highlighter -= 2;
+                    }
+                    else if (touch.Key == ConsoleKey.Escape)
+                    {
+                        choice = false;
+                        playing = false;
+                    }
+                    ChoosingTimer();
+                    Console.WriteLine(@"    Press ""Enter"" to start or ""Escape"" to quit!");
+
+                }
                 playing = false;
             }
+
+        }
+
+        public static void ChoosingTimer()
+        {
+            Console.Clear();
+            if (highlighter == 0)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                if (9 < minute)
+                    Console.Write(minute);
+                else
+                    Console.Write("0" + minute);
+                Console.BackgroundColor = ConsoleColor.Black;
+                if (9 < second)
+                    Console.Write(":" + second);
+                else
+                    Console.Write(":0" + second);
+
+            }
+            else
+            {
+                if (9 < minute)
+                    Console.Write(minute + ":");
+                else
+                    Console.Write("0" + minute + ":");
+                Console.BackgroundColor = ConsoleColor.Blue;
+                if (9 < second)
+                    Console.Write(second);
+                else
+                    Console.Write("0" + second);
+                Console.BackgroundColor = ConsoleColor.Black;
+
+
+            }
+
         }
     }
 }
